0061321e7bf373d038578e569336448d0d412b7f
Slewog <tibs-rps4101@hotmail.com>
2023-12-29 15:10:02 +0100

    Update sprites.py
    Player have just one sprite group.
    Store  paddle width and height in player class.
    
    Change args for Ball __init__
    Add last_wall attr for ball (prevent the ball stay sticky to a border).
    
    Add a reset methods when ball touche wall left or right.
    
    Implement Y velocity from difference in Y with ball and player

--- sprites.py
+++ sprites.py
@@ -10,8 +10,8 @@ class Player(pg.sprite.Sprite):
     VELOCITY = PlayerSettings.VELOCITY
     WALL_OFFSET = PlayerSettings.WALL_OFFSET
 
-    def __init__(self, side:str, screen_w:int, screen_h:int, groups:pg.sprite.Group) -> None:
-        super().__init__(groups)
+    def __init__(self, side:str, screen_w:int, screen_h:int, group:pg.sprite.Group) -> None:
+        super().__init__(group)
 
         # Screen info for collisions.
         self.screen_w = screen_w
@@ -20,14 +20,16 @@ class Player(pg.sprite.Sprite):
         self.direction = pg.math.Vector2()
         self.side = side
         self.score = int(0)
+        self.width = PlayerSettings.WIDTH
+        self.height = PlayerSettings.HEIGHT
 
         if self.side == 'left':
             self.default_pos = (10, self.screen_h//2)
         else:
-            self.default_pos = (self.screen_w - PlayerSettings.WIDTH - 10, self.screen_h//2)
+            self.default_pos = (self.screen_w - self.width - 10, self.screen_h//2)
 
         # Create the paddle (player).
-        self.image = pg.Surface((PlayerSettings.WIDTH, PlayerSettings.HEIGHT))
+        self.image = pg.Surface((self.width, self.height))
         self.image.fill(WHITE)
 
         self.rect = self.image.get_rect(midleft=self.default_pos)
@@ -44,8 +46,7 @@ class Player(pg.sprite.Sprite):
                 self.direction.y = 1
             else:
                 self.direction.y = 0
-
-        if self.side == 'right':
+        elif self.side == 'right':
             if keys[pg.K_UP]:
                 self.direction.y = -1
             elif keys[pg.K_DOWN]:
@@ -78,18 +79,20 @@ class Player(pg.sprite.Sprite):
 class Ball(pg.sprite.Sprite):
     VEL_MULTIPLIER = BallSettings.VEL_MULTIPLIER
 
-    def __init__(self, screen_w:int, screen_h:int, group:pg.sprite.Group, players:list[Player]) -> None:
+    def __init__(self, screen_w:int, screen_h:int, group:pg.sprite.Group, player_left:Player, player_right:Player) -> None:
         super().__init__(group)
 
         # Screen info and players for collisions.
         self.screen_w = screen_w
         self.screen_h = screen_h
-        self.players = players
+        self.player_left = player_left
+        self.player_right = player_right
+        self.last_wall = str("")
 
         # Movement setup.
         self.vel_x = BallSettings.MAX_VELOCITY
-        self.vel_y = self.vel_x -1 
-        self.direction = pg.math.Vector2(0, 0) # choice((1, -1))
+        self.vel_y = float(0) 
+        self.direction = pg.math.Vector2(choice((1, -1)), 1)
         self.default_pos = (screen_w //2, screen_h // 2)
 
         # Get ball size.
@@ -110,32 +113,55 @@ class Ball(pg.sprite.Sprite):
         self.old_rect = self.rect.copy()
 
         self.active = False
-    
+
+    def reset(self) -> None:
+        self.rect.center = self.default_pos
+        self.pos = pg.math.Vector2(self.rect.topleft)
+        self.old_rect = self.rect.copy()
+
+        self.direction.y = 1
+        self.direction.x *= -1
+        self.vel_y = float(0)
+        self.last_wall = str("")
+
+    def calcule_vel_y(self, sprite:Player) -> None:
+        # Calcule new Y vel from difference in Y pos with the ball and a player paddle.
+        max_vel = float(self.vel_x - 1) # Remove one from max vel to get a smooth movement from ball.
+        difference_in_y = float(sprite.rect.center[1] - self.rect.center[1])
+        reduction_factor = float((sprite.height / 2) / max_vel)
+        self.vel_y = (difference_in_y / reduction_factor)  
+
+    def calcule_speed(self, vel:int, dt:float) -> float:
+        return (vel * 65) * dt  
+
     def display_collisions(self, direction:str) -> None:
         if direction == 'vertical':
-            if self.rect.top < 0:
+            if self.rect.top < 0 and self.last_wall != 'top':
                 self.rect.top = 0
                 self.pos.y = self.rect.y
                 self.direction.y *= -1
+                self.last_wall = str("top")
 
-            if self.rect.bottom > self.screen_h:
+            elif self.rect.bottom > self.screen_h and self.last_wall != 'bottom':
                 self.rect.bottom = self.screen_h
                 self.pos.y = self.rect.y
                 self.direction.y *= -1
+                self.last_wall = str("bottom")
+            else:
+                self.last_wall = str("")
 
         if direction == 'horizontal':
-            if self.rect.left < 0:
-                self.rect.left = 0
-                self.pos.x = self.rect.x
-                self.direction.x *= -1
-            
-            if self.rect.right > self.screen_w:
-                self.rect.right = self.screen_w
-                self.pos.x = self.rect.x
-                self.direction.x *= -1
+            if self.rect.left < 0 or self.rect.right > self.screen_w:
+                self.reset()
 
     def collisions(self, direction:str):
-        overlap_sprites:list[pg.sprite.Sprite] = pg.sprite.spritecollide(self, self.players, False)
+        overlap_sprites:list[pg.sprite.Sprite] = []
+
+        if self.rect.colliderect(self.player_left.rect):
+            overlap_sprites.append(self.player_left)
+        
+        if self.rect.colliderect(self.player_right.rect):
+            overlap_sprites.append(self.player_right)
 
         if overlap_sprites:
             if direction == 'horizontal':
@@ -144,11 +170,15 @@ class Ball(pg.sprite.Sprite):
                         self.rect.right = sprite.rect.left - 1
                         self.pos.x = self.rect.x
                         self.direction.x *= -1
+                        self.direction.y *= -1
+                        self.calcule_vel_y(sprite)
                     
                     if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
                         self.rect.left = sprite.rect.right + 1
                         self.pos.x = self.rect.x
                         self.direction.x *= -1
+                        self.direction.y *= -1
+                        self.calcule_vel_y(sprite)
             
             if direction == 'vertical':
                 for sprite in overlap_sprites:
@@ -164,9 +194,6 @@ class Ball(pg.sprite.Sprite):
     
         self.display_collisions(direction)
 
-    def calcule_speed(self, vel:int, dt:float) -> float:
-        return (vel * 65) * dt
-    
     def update(self, dt:float) -> None:
         # Check delta time.
         if dt is None or type(dt) != float:
@@ -184,6 +211,6 @@ class Ball(pg.sprite.Sprite):
             self.collisions('horizontal')
 
         if self.direction.y != 0:
-            self.pos.y += self.direction.y * self.calcule_speed(self.vel_y - 1, dt)
+            self.pos.y += self.direction.y * self.calcule_speed(self.vel_y, dt)
             self.rect.y = round(self.pos.y)
             self.collisions('vertical')
