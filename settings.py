8cc15e6d95fbbe03f49ad9dbc637f955da7d6c67
Slewog <tibs-rps4101@hotmail.com>
2023-12-29 04:01:17 +0100

    Update settings.py
    add MAX_SCORE to GameSettings
    
    update BallSettings

--- settings.py
+++ settings.py
@@ -1,11 +1,12 @@
 class GameSettings:
     SCREEN_W = int(1280)
     SCREEN_H = int(720)
-
     USE_FPS = bool(False)
     FPS = int(60)
+    MAX_SCORE = int(5)
 
 
 class BallSettings:
-    RADIUS = int(16)
-    VELOCITY = int(300)
+    RADIUS = int(14)
+    MAX_VELOCITY = int(8)
+    VEL_MULTIPLIER = int(65)
