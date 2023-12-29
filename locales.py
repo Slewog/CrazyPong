e00645affb5f5f3a1fa523ab6e8dd8bb3e18b7ae
Slewog <tibs-rps4101@hotmail.com>
2023-12-30 00:59:28 +0100

    Add new locales

--- locales.py
+++ locales.py
@@ -1,2 +1,8 @@
 class Locales:
     game_name = "Py Pong"
+    START_TXT = "Appuyer sur ESPACE pour jouer"
+    RESTART_TXT = "Appuyer sur ESPACE pour re-jouer"
+
+    WIN_TXT = "Le joueur {player} à gagner"
+    PLAYER_LEFT = "gauche"
+    PLAYER_RIGHT = "droit"
