899878fd45de646dfe6384065eb55d5e364978ba
Slewog <tibs-rps4101@hotmail.com>
2023-12-31 19:37:28 +0100

    Update typing

--- locales.py
+++ locales.py
@@ -1,8 +1,8 @@
 class Locales:
-    GAME_NAME = "Py Pong"
-    START_TXT = "Appuyer sur ESPACE pour jouer"
-    RESTART_TXT = "Appuyer sur ESPACE pour re-jouer"
+    GAME_NAME = str("Py Pong")
+    START_TXT = str("Appuyer sur ESPACE pour jouer")
+    RESTART_TXT = str("Appuyer sur ESPACE pour re-jouer")
 
-    WIN_TXT = "Le joueur {player} à gagner"
-    PLAYER_LEFT = "gauche"
-    PLAYER_RIGHT = "droit"
+    WIN_TXT = str("Le joueur {player} à gagner")
+    PLAYER_LEFT = str("gauche")
+    PLAYER_RIGHT = str("droit")
