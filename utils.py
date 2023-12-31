533743422f16d54c4860d3bd4ea391d0c722e46f
Slewog <tibs-rps4101@hotmail.com>
2023-12-31 19:37:16 +0100

    Refactor and typing

--- utils.py
+++ utils.py
@@ -11,15 +11,16 @@ def resource_path(directory:str, resource:str) -> str:
     """
     return path.join(MAIN_DIR, directory, resource)
 
-def load_sound(sound:str, vol:float=1.0):
+def load_sound(file:str, vol:float=1.0) -> pg.mixer.Sound:
     if not pg.mixer or not pg.mixer.get_init():
         return NoneSound()
     
-    try:
-        loaded_sound = pg.mixer.Sound(resource_path("assets/sounds", sound))
-    except FileNotFoundError:
+    d_path = str("assets/sounds")
+    sound_path = resource_path(d_path, file)
+    if not path.exists(sound_path):
         return NoneSound()
     
+    loaded_sound = pg.mixer.Sound(sound_path)
     loaded_sound.set_volume(vol)
     return loaded_sound
 
