f03c14bc2536802a16c79bd0968c5ef6ce09d3bd
Slewog <tibs-rps4101@hotmail.com>
2023-12-31 13:57:23 +0100

    Add new settings

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 class GameSettings:
+    DEBUG = bool(False)
     SCREEN_W = int(1280)
     SCREEN_H = int(960)
     USE_FPS = bool(True)
@@ -7,20 +8,29 @@ class GameSettings:
     BACKGROUND_COLOR = str('gray12') # 'gray12' or '#2F373F'
     FONT_COLOR = str('gray79') # (27,35,43) or str('gray79')
     OBJECT_COLOR = str('white')
-    HIT_SOUND_VOL = float(0.6)
-    SCORE_SOUND_VOL = float(0.6)
+    HIT_SOUND_VOL = float(0.0)
+    SCORE_SOUND_VOL = float(0.0)
 
 
 class BallSettings:
     DEBUG = bool(True)
     RADIUS = int(14)
-    MAX_VELOCITY = int(8)
+    MAX_VELOCITY = int(7)
     Y_VEL_RAND = bool(False)
 
 
 class PlayerSettings:
     WIDTH = int(12)
-    HEIGHT = int(220)
-    VELOCITY = int(900)
+    HEIGHT = int(180)
+    VELOCITY = int(500)
     WALL_OFFSET = int(10)
     SCORE_Y_POS = int(20)
+
+
+class DebugSettings:
+    WIDTH = int(200)
+    HEIGHT = int(155)
+    X_POS = int(30)
+    Y_POS = int(20)
+    FONT_SIZE = int(25)
+    BORDER_RADIUS = int(10)
