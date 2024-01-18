b130cd33e74b894332222b248ff21ce6082f5cd5
Slewog <tibs-rps4101@hotmail.com>
2024-01-18 21:44:48 +0100

    add menu settings

--- src/const/settings.py
+++ src/const/settings.py
@@ -11,6 +11,12 @@ FONT = {
     'size': int(40)
 }
 
+MENU = {
+    'copyright': "Create by Slewog",
+    'title': GAME['name'],
+    'btn_radius': int(12)
+}
+
 FONT_COLOR = (27, 35, 43)
 OBJECT_COLOR = str('#27333E')
 BACKGROUND_COLOR = str('#46505A')
