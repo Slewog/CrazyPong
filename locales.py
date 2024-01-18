23787ceccb8c270caab0c3f99f3566ab146bc9dd
Slewog <tibs-rps4101@hotmail.com>
2024-01-18 19:10:28 +0100

    delete old code

--- locales.py
+++
@@ -1,8 +0,0 @@
-class Locales:
-    GAME_NAME = str("Py Pong")
-    START_TXT = str("Appuyer sur ESPACE pour jouer")
-    RESTART_TXT = str("Appuyer sur ESPACE pour re-jouer")
-
-    WIN_TXT = str("Le joueur {player} à gagner")
-    PLAYER_LEFT = str("gauche")
-    PLAYER_RIGHT = str("droit")
