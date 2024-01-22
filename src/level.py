98cad422703a33de025534800961a9ae6f169de3
Slewog <tibs-rps4101@hotmail.com>
2024-01-22 17:02:36 +0100

    Create level.py

---
+++ src/level.py
@@ -0,0 +1,146 @@
+import pygame as pg
+
+from .objects.paddle import Paddle
+from .objects.ball import Ball
+
+class Level:
+    FONT: pg.font.Font
+    FONT_COLOR: pg.Color
+    BG_COLOR: pg.Color
+
+    SCREEN_MW: int
+    SCREEN_MH: int
+    SCREEN_W_QUART: int
+
+    SCORE_OFFSET_Y: int
+    COUNT_BG_OFFSET: int
+    COUNTER_OFFSET_Y: int
+
+    # RESTART_TXT: pg.Surface
+    # RESTART_TXT_RECT: pg.Rect
+
+    def __init__(self, level_type: str, debug) -> None:
+        self.ball_group = pg.sprite.GroupSingle()
+        self.paddles_group = pg.sprite.Group()
+
+        self.winned = bool(False)
+        self.started = bool(False)
+        self.counter = int(-1)
+        self.reset_time = int(0)
+
+        self.ball = Ball(self.ball_group)
+        self.paddles = [
+            Paddle('left', 'player', self.SCREEN_W_QUART, self.paddles_group),
+            Paddle('right', level_type == 'oneplayer' and 'ai' or 'player', self.SCREEN_W_QUART, self.paddles_group)
+        ]
+
+        for paddle in self.paddles:
+            score_txt = self.FONT.render(str(paddle.score), True, self.FONT_COLOR)
+            paddle.score_txt = score_txt
+            paddle.score_pos = (paddle.hud_pos_x - (score_txt.get_rect().width // 2), self.SCORE_OFFSET_Y)
+
+    def reset(self):
+        for paddle in self.paddles:
+            paddle.reset()
+            self.update_paddle_hud(paddle)
+        
+        self.winned = bool(False)
+        self.reset_time = pg.time.get_ticks()
+
+    def update_paddle_hud(self, paddle: Paddle):
+        score_txt = self.FONT.render(str(paddle.score), True, self.FONT_COLOR)
+        paddle.score_txt = score_txt
+        paddle.score_pos = (paddle.hud_pos_x - (score_txt.get_rect().width // 2), self.SCORE_OFFSET_Y)
+    
+    def start(self):
+        self.started = not self.started
+        self.reset_time = pg.time.get_ticks()
+
+    def destroy(self) -> None:
+        self.ball.kill()
+
+        for paddle in self.paddles:
+            paddle.kill()
+    
+    def counter_active(self):
+        return self.reset_time != 0
+    
+    def add_point_to_paddle(self, target: str) -> None:
+        """target is the side of the paddle targetted"""
+        if type(target) != str:
+            return
+
+        for paddle in self.paddles:
+            if paddle.side == target:
+                paddle.add_point()
+                self.update_paddle_hud(paddle)
+                winned = paddle.winned()
+                if paddle.score < paddle.MAX_SCORE:
+                    self.reset_time = pg.time.get_ticks()
+
+                if winned:
+                    self.winned = winned
+                
+                self.ball.reset(self.winned)
+    
+    def update_counter(self, value: int) -> None:
+        self.counter = value
+        # Create a new counter text surface on update.
+        self.counter_txt = self.FONT.render(
+            str(self.counter), True, self.FONT_COLOR)
+        
+        self.counter_rect = self.counter_txt.get_rect(
+            midbottom = (self.SCREEN_MW, self.ball.rect.top - self.COUNTER_OFFSET_Y)
+        )
+
+        self.counter_bg = pg.Rect(
+            self.counter_rect.x,
+            self.counter_rect.y - self.COUNT_BG_OFFSET,
+            self.counter_rect.width,
+            self.counter_rect.height
+        )
+
+    def check_counter(self):
+        current_time = pg.time.get_ticks()
+        reset_time = current_time - self.reset_time
+
+        if reset_time <= 700 and self.counter != 3:
+            self.update_counter(3)
+        if 700 < reset_time <= 1400 and self.counter != 2:
+            self.update_counter(2)
+        if 1400 < reset_time <= 2100 and self.counter != 1:
+            self.update_counter(1)
+        if reset_time >= 2100:
+            self.ball.set_active(True)
+            self.freeze_time = int(0)
+            self.update_counter(-1)
+
+    def render_frame(self, display_surf: pg.Surface) -> None:
+        self.paddles_group.draw(display_surf)
+        self.ball_group.draw(display_surf)
+
+        for paddle in self.paddles:
+            display_surf.blit(paddle.score_txt, paddle.score_pos)
+
+        if self.winned:
+            # Render text to said who win.
+            # Render text or button to restart or leave.
+            pass
+
+        if self.started and not self.winned and not self.ball.active and self.counter_active():
+            pg.draw.rect(display_surf, self.BG_COLOR, self.counter_bg)
+            display_surf.blit(self.counter_txt, self.counter_rect)
+
+    def run(self, display_surf: pg.Surface, dt: float) -> None:
+        if not self.winned:
+            keys = pg.key.get_pressed()
+            for paddle in self.paddles:
+                paddle.check_input(keys)
+            
+            if not self.ball.active and self.counter_active():
+                self.check_counter()
+
+        self.paddles_group.update(dt, self.ball)
+        self.ball_group.update(dt, self.paddles)
+
+        self.render_frame(display_surf)
